0d422c9d342a2d58d2b7455e5ba48d3bd3a741c8
unknown <noahnolte777@gmail.com>
2021-01-19 23:35:35 -0600

    come on man!

--- grapher.py
+++ grapher.py
@@ -1,7 +1,15 @@
 from matplotlib import pyplot as plt
+import json
 
-y = [x for x in range(-100, 100)]
-print(y)
-plt.plot(y)
-plt.axhline(y=90, color='r', linestyle='-')
+# y = [x for x in range(-100, 100)]
+# y = input("> ")
+# y = y.splitlines()
+# x = json.loads(y)
+
+with open("values.json", 'r') as jsonfile:
+    jsondict = json.load(jsonfile)
+
+print(jsondict["values"])
+plt.plot(jsondict["values"])
+plt.axhline(y=jsondict["desired"], color='r', linestyle='-')
 plt.show()
